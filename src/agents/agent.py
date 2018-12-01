97fc916417ad7b95e294b0bbd665c3533392f420
Milosz Blasiak <milblasiak@gmail.com>
2018-12-01 21:50:51 +0100

    Add methods for retrieving necessary information
    Add and implement methods retrieving necessary information from
    directions_map like angle, step_size, next_desired_location
    
    Correct class constructor

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -1,28 +1,53 @@
+import numpy
 import numpy as np
 
 from environment import a_star
 
 
 class Agent:
-    start_position = None
-    end_position = None
-    max_step = 1
-    front_collision_size = None
-    rear_collision_size = None
-    direction_map = None
-    free_space_map = None
 
     def __init__(self, start_position, end_postion, max_step, front_collison_size, rear_collision_size, directions_map,
-                 free_space_map):
+                 collision_map):
+
         self.start = start_position
         self.end = end_postion
+        self.current_pos = self.start
         self.max_step = max_step
+
         self.front_collision_size = front_collison_size
         self.rear_collision_size = rear_collision_size
         self.direction_map = directions_map
+        self.collision_map = collision_map
+        self.map_x_size = len(directions_map)
+        self.map_y_size = len(directions_map[0])
 
-    def get_prefered_direction(self):
-        pass
+    def get_next_desired_location(self):
+        (current_x, current_y) = self.current_pos
+        return self.direction_map[current_x][current_y]
+
+    def get_desired__direction(self):
+        return self.get_next_desired_location() - self.current_pos
+
+    def get_desired_step_size(self):
+        (desired_x, desired_y) = self.get_desired__direction()
+
+        # Return distance beetwen current location and desired location
+        return (desired_x ** 2 + desired_y ** 2) ** 0.5
+
+    def get_desired__direction_engle(self):
+        # y/x=tg
+        # alpha=arctg(y/x)
+
+        (desired_x, desired_y) = self.get_desired__direction()
+
+        # if x=0 cant use arctg
+        if (desired_x == 0):
+            if (y >= 0):
+                return np.pi / 2
+            else:
+                return np.pi * 1.5
+
+        return np.arctan(desired_y / desired_x)
 
     def get_avaialbe_moves(self):
         pass
@@ -34,6 +59,7 @@ class Agent:
         pass
 
     def update_collision_map(self):
+
         pass
 
     def clear_last_postionion_from_collision_map(self):
@@ -44,8 +70,6 @@ class Agent:
 
     def move(self):
         pass
-
-
 
     def check_if_finish(self):
         if self.end == self.start:
