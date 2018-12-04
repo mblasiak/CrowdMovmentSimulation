7fa45e4d30058d7681c64f18208126ee07c80007
Milosz Blasiak <milblasiak@gmail.com>
2018-12-04 01:17:32 +0100

    Start testing Agnet movment

--- tests/alpha_tests/walking_test.py
+++ tests/alpha_tests/walking_test.py
@@ -1,13 +1,15 @@
 from src.agent.Agent import Agent
 from src.direction_map.DirectionMap import DirectionMap
-from src.environment.environment import map_environment
-star_pos=(0,0)
-end_pos=(99,99)
-collisions=[[0]*10]*10
-directions=map_environment(collisions,end_pos)
+from src.environment.environment import map_environment, direction_map
+from src.environment.line import Point
 
+star_pos = (1, 1)
+end_pos = (99, 99)
+collisions = [[0] * 100] * 100
 
-
-print(collisions)
-
-
+directions = direction_map(collisions, [Point(30, 20)], 3)
+print(directions)
+direct = DirectionMap(directions)
+human_0 = Agent(star_pos, end_pos, 3, 3, 2, direct, collisions)
+human_0.move()
+human_0.move()
