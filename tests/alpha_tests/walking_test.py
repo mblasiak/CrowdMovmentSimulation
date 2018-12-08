d52cd83d4670d630890ac477b547ac1c5891b893
Milosz Blasiak <milblasiak@gmail.com>
2018-12-08 16:22:52 +0100

    Create code breaking path finding

--- tests/alpha_tests/walking_test.py
+++ tests/alpha_tests/walking_test.py
@@ -1,15 +1,34 @@
 from src.agent.Agent import Agent
 from src.direction_map.DirectionMap import DirectionMap
-from src.environment.environment import map_environment, direction_map
+from src.environment.environment import direction_map
 from src.environment.line import Point
 
 star_pos = (1, 1)
 end_pos = (99, 99)
-collisions = [[0] * 100] * 100
 
-directions = direction_map(collisions, [Point(30, 20)], 3)
+collisions = [[0] * 100] * 100
+collisions[10][10] = 1
+collisions[11][10] = 1
+
+collisions[1][11]=1
+collisions[1][12]=1
+
+print("Mapa colizji: {}".format(collisions))
+
+
+directions = direction_map(collisions, [Point(7, 7)], 3)
 print(directions)
-direct = DirectionMap(directions)
-human_0 = Agent(star_pos, end_pos, 3, 3, 2, direct, collisions)
-human_0.move()
-human_0.move()
+
+
+
+
+# direct = DirectionMap(directions)
+# human_0 = Agent(star_pos, end_pos, 3, 3, 2, direct, collisions)
+# human_0.move()
+# human_0.move()
+# human_0.move()
+# human_0.move()
+# human_0.move()
+# human_0.move()
+# human_0.move()
+# human_0.move()
