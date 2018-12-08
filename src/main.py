caa6271850a0a33a6b8dd8656ba40fc280ecaf20
Milosz Blasiak <milblasiak@gmail.com>
2018-12-08 15:57:45 +0100

    Remove use of map_enviroment() and it's import
    Remove coz map_enviroment has been removed from class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-from environment.a_star import astar
-from environment.environment import map_environment, get_obstacle_line_horizon, get_obstacle_line_vertical, \
-    direction_map
-from environment.line import Point, Line
+from src.environment.environment import direction_map
+from src.environment.line import Point, Line
 
-maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+maze = [[0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
@@ -14,10 +12,6 @@ maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-
-print(get_obstacle_line_horizon(maze))
-print(get_obstacle_line_vertical(maze))
-
 p1 = Point(0, 0)
 p2 = Point(6, 0)
 
@@ -26,8 +20,6 @@ p3 = Point(4, 0)
 l1 = Line(p1, p2)
 l2 = Line(p3, p3)
 
-for ele in map_environment(maze, [(9, 1)]):
-    print(ele)
-
+print("")
 for ele in direction_map(maze, [Point(1, 9), Point(2, 9)], 3):
     print(ele)
