54e47a188b4f7705cc42eff518ee590f1d20f61a
Milosz Blasiak <milblasiak@gmail.com>
2018-12-08 14:29:57 +0100

    Create runner file to run test from one place

---
+++ tests/unit_tests/run_tests.py
@@ -0,0 +1,10 @@
+import unittest
+import tests.unit_tests.navigator_tests.NavigationTest as nav
+
+
+if __name__ == '__main__':
+    a=nav.TestAngle
+    b=nav.TestDirection
+    c=nav.TestDistance
+
+    unittest.main()
