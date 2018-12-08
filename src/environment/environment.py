519811064a1f709857768f5f8a6ba61292a7f511
Milosz Blasiak <milblasiak@gmail.com>
2018-12-08 15:55:46 +0100

    Remove map_environment, because is redundant code
    map_enviroment is included in direction_map so now map_enviroment is
    redundant

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -100,42 +100,6 @@ def get_obstacle_line_horizon(environment):
     return lines
 
 
-def map_environment(environment, exit_points):
-    """ Return mapped environment, each cord has next cord in 'fastest' path to exit (for step_size = 1)"""
-    """ its should be deleted because we have direciton map, but i will delete it in next push """
-    mapped_environment = copy.deepcopy(environment)
-    for i in range(0, len(mapped_environment)):
-        for j in range(0, len(mapped_environment[i])):
-            if mapped_environment[i][j] == 1:
-                mapped_environment[i][j] = Env.OBSTACLE
-            else:
-                mapped_environment[i][j] = None
-
-    for y in range(0, len(environment)):
-        for x in range(0, len(environment[y])):
-            if mapped_environment[y][x] is None:
-                fastest_paths = []
-                for point in exit_points:
-                    fastest_paths.append(astar(environment, (y, x), point))
-
-                shortest_path = fastest_paths[0]
-                distance_of_shortest_path = path_distance(shortest_path)
-                for path in fastest_paths:
-                    if distance_of_shortest_path > path_distance(path):
-                        shortest_path = path
-                        distance_of_shortest_path = path_distance(path)
-
-                for i in range(0, len(shortest_path)):
-                    current_x = shortest_path[i][1]
-                    current_y = shortest_path[i][0]
-                    if i == len(shortest_path)-1:
-                        mapped_environment[current_y][current_x] = Env.EXIT
-                    else:
-                        mapped_environment[current_y][current_x] = shortest_path[i+1]
-
-    return mapped_environment
-
-
 def path_distance(path):
 
     distance = 0
