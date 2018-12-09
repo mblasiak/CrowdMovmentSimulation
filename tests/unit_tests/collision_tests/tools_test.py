aa12aa8a3fc5b1e9f0bcbbf3f4d3c186b0ea4097
Milosz Blasiak <milblasiak@gmail.com>
2018-12-09 13:19:42 +0100

    Add collision_map marking tests

--- tests/unit_tests/collision_tests/tools_test.py
+++ tests/unit_tests/collision_tests/tools_test.py
@@ -10,32 +10,32 @@ class TestMarking(unittest.TestCase):
 
     def setUp(self):
 
-        self.collision_map = np.zeros((5, 5)).tolist()
+        self.collision_map = np.zeros((100, 100)).tolist()
 
     def test_mark_position(self):
-        pass
-        # desired_result =copy.deepcopy(self.collision_map)
-        # desired_result[2][1] = 1
-        # print(desired_result)
-        # mark_location_as_taken((1, 2), self.collision_map, 1)
-        # print(self.collision_map)
-        # self.assertTrue(self.compare_elements(desired_result, self.collision_map))
+        desired_result =copy.deepcopy(self.collision_map)
 
-    # def test_mark_far_position(self):
-    #     desired_result = copy.deepcopy(self.collision_map)
-    #     desired_result[35][88] = 1
-    #
-    #     mark_location_as_taken((35, 88), self.collision_map, 1)
-    #
-    #     self.assertEqual(desired_result, self.collision_map)
-    #
-    # def test_doesnt_allways_equal(self):
-    #     desired_result = copy.deepcopy(self.collision_map)
-    #     desired_result[35][88] = 1
-    #
-    #     mark_location_as_taken((30, 88), self.collision_map, 1)
-    #
-    #     self.assertEqual(desired_result, self.collision_map)
+        desired_result[2][2] = 1
+
+        mark_location_as_taken((2, 2), self.collision_map, 1)
+
+        self.assertTrue(self.compare_elements(desired_result, self.collision_map))
+
+    def test_mark_far_position(self):
+        desired_result = copy.deepcopy(self.collision_map)
+        desired_result[35][88] = 1
+
+        mark_location_as_taken((35, 88), self.collision_map, 1)
+
+        self.assertTrue(self.compare_elements(desired_result,self.collision_map))
+
+    def test_doesnt_allways_equal(self):
+        desired_result = copy.deepcopy(self.collision_map)
+        desired_result[35][88] = 1
+
+        mark_location_as_taken((30, 88), self.collision_map, 1)
+
+        self.assertFalse(self.compare_elements(desired_result,self.collision_map))
 
     def compare_elements(self, a, b):
         for x in range(0, len(a)):
