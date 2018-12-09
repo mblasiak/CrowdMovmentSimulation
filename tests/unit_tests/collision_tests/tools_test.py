b62d1e0a69fe8198f62d7a1075dc948a138d0583
Milosz Blasiak <milblasiak@gmail.com>
2018-12-09 14:12:08 +0100

    Correct not iterating the last elements

--- tests/unit_tests/collision_tests/tools_test.py
+++ tests/unit_tests/collision_tests/tools_test.py
@@ -3,23 +3,22 @@ import unittest
 
 import numpy as np
 
-from src.collisions.collision_map_tools import mark_location_as_taken
+from src.collisions.collision_map_tools import add_squere_obstacle, mark_location_as_taken, add_circle_obstacle
 
 
 class TestMarking(unittest.TestCase):
 
     def setUp(self):
-
         self.collision_map = np.zeros((100, 100)).tolist()
 
     def test_mark_position(self):
-        desired_result =copy.deepcopy(self.collision_map)
+        desired_result = copy.deepcopy(self.collision_map)
 
         desired_result[2][2] = 1
 
         mark_location_as_taken((2, 2), self.collision_map, 1)
 
-        self.assertTrue(self.compare_elements(desired_result, self.collision_map))
+        self.assertTrue(compare_elements(desired_result, self.collision_map))
 
     def test_mark_far_position(self):
         desired_result = copy.deepcopy(self.collision_map)
@@ -27,7 +26,7 @@ class TestMarking(unittest.TestCase):
 
         mark_location_as_taken((35, 88), self.collision_map, 1)
 
-        self.assertTrue(self.compare_elements(desired_result,self.collision_map))
+        self.assertTrue(compare_elements(desired_result, self.collision_map))
 
     def test_doesnt_allways_equal(self):
         desired_result = copy.deepcopy(self.collision_map)
@@ -35,13 +34,66 @@ class TestMarking(unittest.TestCase):
 
         mark_location_as_taken((30, 88), self.collision_map, 1)
 
-        self.assertFalse(self.compare_elements(desired_result,self.collision_map))
+        self.assertFalse(compare_elements(desired_result, self.collision_map))
 
-    def compare_elements(self, a, b):
-        for x in range(0, len(a)):
-            for y in range(0, len(a[1])):
+    def test_return_true_when_marking_in_range(self):
+        self.assertTrue(mark_location_as_taken((30, 88), self.collision_map, 1))
 
-                if (a[x][y]) != (b[x][y]):
-                    return False
+    def test_return_false_when_markoing_out_of_range(self):
+        self.assertFalse(mark_location_as_taken((120, 99), self.collision_map, 1))
 
-        return True
+    def test_do_not_mark_when_out_of_range(self):
+        desired_result = copy.deepcopy(self.collision_map)
+
+        mark_location_as_taken((300, 88), self.collision_map, 1)
+
+        self.assertTrue(compare_elements(desired_result, self.collision_map))
+
+
+class ObstacleAdding(unittest.TestCase):
+
+    def setUp(self):
+        self.collision_map = np.zeros((100, 100)).tolist()
+
+    def test_marking_squere(self):
+        desired_result = copy.deepcopy(self.collision_map)
+
+        desired_result[2][2] = 1
+        desired_result[2][1] = 1
+        desired_result[2][3] = 1
+
+        desired_result[1][2] = 1
+        desired_result[1][1] = 1
+        desired_result[1][3] = 1
+
+        desired_result[3][1] = 1
+        desired_result[3][2] = 1
+        desired_result[3][3] = 1
+
+        add_squere_obstacle((2, 2), 2, self.collision_map, 1)
+
+        self.assertTrue(compare_elements(desired_result, self.collision_map))
+
+    def test_marking_circle(self):
+        desired_result = copy.deepcopy(self.collision_map)
+
+        desired_result[2][2] = 1
+        desired_result[2][1] = 1
+        desired_result[2][3] = 1
+
+        desired_result[1][2] = 1
+
+        desired_result[3][2] = 1
+
+        add_circle_obstacle((2, 2), 1, self.collision_map, 1)
+        self.assertTrue(compare_elements(desired_result, self.collision_map))
+
+
+def compare_elements(a, b):
+    for x in range(0, len(a)):
+        for y in range(0, len(a[1])):
+
+            if (a[x][y]) != (b[x][y]):
+                return False
+
+    return True
