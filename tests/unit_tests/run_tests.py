47fe5db3731bfe1132bc275109f8449f4b00858c
Milosz Blasiak <milblasiak@gmail.com>
2018-12-09 19:13:58 +0100

    Add new unit tests to test runner

--- tests/unit_tests/run_tests.py
+++ tests/unit_tests/run_tests.py
@@ -1,10 +1,23 @@
 import unittest
 import tests.unit_tests.navigator_tests.NavigationTest as nav
+import tests.unit_tests.collision_tests.tools_test as tools
+import tests.unit_tests.direction_map_tests.DirectionMapTests as dm
 
+#Run this file to run all unit tests
+#Other python running methods aren't much more better in my opinion
 
 if __name__ == '__main__':
-    a=nav.TestAngle
-    b=nav.TestDirection
-    c=nav.TestDistance
+
+    nav_1 = nav.TestAngle
+    nav_2 = nav.TestDirection
+    nav_3 = nav.TestDistance
+
+    tool_1=tools.ObstacleAdding
+    tool_2=tools.TestMarking
+
+    dirmap_1=dm.GetingAngleTest
+    dirmap_2=dm.GettingDirectionTests
+    dirmap_3=dm.GettingPosTests
+    dirmap_4=dm.GettingStepSizeTests
 
     unittest.main()
