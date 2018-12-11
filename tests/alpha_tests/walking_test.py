633b6f7e61f3608e9880f284296bf39601df5c93
Milosz Blasiak <milblasiak@gmail.com>
2018-12-11 17:56:54 +0100

    Remove debugging prints(), leave necessary code

--- tests/alpha_tests/walking_test.py
+++ tests/alpha_tests/walking_test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from src.agent.Agent import Agent
 from src.direction_map.DirectionMap import DirectionMap
 from src.environment.environment import direction_map
@@ -6,29 +8,18 @@ from src.environment.line import Point
 star_pos = (1, 1)
 end_pos = (99, 99)
 
-collisions = [[0] * 100] * 100
-collisions[10][10] = 1
-collisions[11][10] = 1
+collisions = np.zeros((100, 100)).tolist()
 
-collisions[1][11]=1
-collisions[1][12]=1
+directions = direction_map(collisions, [Point(67, 70)], 3)
 
-print("Mapa colizji: {}".format(collisions))
+direct = DirectionMap(directions)
 
-
-directions = direction_map(collisions, [Point(7, 7)], 3)
-print(directions)
-
-
-
-
-# direct = DirectionMap(directions)
-# human_0 = Agent(star_pos, end_pos, 3, 3, 2, direct, collisions)
-# human_0.move()
-# human_0.move()
-# human_0.move()
-# human_0.move()
-# human_0.move()
-# human_0.move()
-# human_0.move()
-# human_0.move()
+human_0 = Agent(star_pos, end_pos, 3, 3, 2, direct, collisions)
+human_0.move()
+human_0.move()
+human_0.move()
+human_0.move()
+human_0.move()
+human_0.move()
+human_0.move()
+human_0.move()
