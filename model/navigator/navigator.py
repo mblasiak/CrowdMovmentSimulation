e5e0380a9118489ebeb7945759510677bf5bb3be
Milosz Blasiak <milblasiak@gmail.com>
2019-01-06 21:49:30 +0100

    Change vars to stop misleading with swapped x y
    Actually code was fine only naming wasn't just accurate

--- model/navigator/navigator.py
+++ model/navigator/navigator.py
@@ -6,10 +6,10 @@ def get_direction_to_another_point(pos_1: (int, int), pos_2: (int, int)):
 
 
 def get_distance_beteween_points(pos_1: (int, int), pos_2: (int, int)):
-    (desired_x, desired_y) = get_direction_to_another_point(pos_1, pos_2)
+    (desired_y, desired_x) = get_direction_to_another_point(pos_1, pos_2)
     return (desired_x ** 2 + desired_y ** 2) ** 0.5
 
 
 def get_angle_of_direction_between_points(pos_1: (int, int), pos_2: (int, int)):
-    (desired_x, desired_y) = get_direction_to_another_point(pos_1, pos_2)
+    (desired_y, desired_x) = get_direction_to_another_point(pos_1, pos_2)
     return np.arctan2(desired_y ,desired_x)
