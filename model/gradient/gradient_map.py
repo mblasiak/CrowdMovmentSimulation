0621744ed7920f8e9ccde29ccf2776770e0f3291
PiotrKedra <piotr.k9750@gmail.com>
2019-01-10 17:29:02 +0100

    gradient map based on direction map is working

--- model/gradient/gradient_map.py
+++ model/gradient/gradient_map.py
@@ -3,45 +3,15 @@ from copy import deepcopy
 from model.environment.environment_enum import Env
 from resources.handling.reading import load_direction_from_file
 
-import numpy
-
-
-def gradient(direction_map_txt: str, ):
-    """ Its supposed to return gradient map with ..."""
-    direction_map = load_direction_from_file(direction_map_txt)
-
-    gradient_map = deepcopy(direction_map)
-
-    source = (50, 110)
-
-    for y in range(0, len(direction_map)):
-        for x in range(0, len(direction_map[y])):
-
-            # Check if we already updated gradient map in [y][x]
-            if gradient_map[y][x] != direction_map[y][x]:
-                continue
-
-            # If obstacle we skip
-            if direction_map[y][x] == Env.OBSTACLE or direction_map[y][x] == Env.EXIT:
-                continue
-
-            point1 = (y, x)
-            point2 = direction_map[y][x]
-            gradient_map[y][x] = numpy.rad2deg(numpy.arctan2((point2[0]-point1[0]), (point2[1]-point1[1])))
-
-            source_angel = numpy.rad2deg(numpy.arctan2((source[0]-point1[0]), (source[1]-point1[1])))
-
-            gradient_map[y][x] = source_angel
-
-
-    return gradient_map
-
 
 def gradient_from_direction_map(direction_map_txt: str):
+    """Returns gradient map based on distance counted from direction map"""
     direction_map = load_direction_from_file(direction_map_txt)
 
+    # Make gradient map same size as direction map
     gradient_map = deepcopy(direction_map)
 
+    # Main loop, loop through all the points
     for y in range(0, len(direction_map)):
         for x in range(0, len(direction_map[y])):
 
@@ -53,24 +23,51 @@ def gradient_from_direction_map(direction_map_txt: str):
             if direction_map[y][x] == Env.OBSTACLE:
                 continue
 
+            # Get current position
             position = direction_map[y][x]
-            distance = 0
-            while position != Env.EXIT:
-                distance += 1
-                position = direction_map[position[0]][position[1]]
 
+            # Initialize distance variable for distance from the start point
+            distance = 0
+
+            # Loop till EXIT found so distance is full
+            while position != Env.EXIT:
+
+                # Get next position
+                next_position = direction_map[position[0]][position[1]]
+
+                if next_position == Env.EXIT:  # TODO its bad break exit, find better solution for this
+                    break
+
+                # Check if we move vertical (+10) or if diagonal (+14 (sqrt(2)*10))
+                if position[0] == next_position[0] or position[1] == next_position[1]:
+                    distance += 10
+                else:
+                    distance += 14
+
+                position = next_position
+
+            # Loop backward and fill gradient map with correct distance
             gradient_map[y][x] = distance
+            # Again get current position
             position = direction_map[y][x]
             while position != Env.EXIT:
-                distance -= 1
+
+                # Get next position
+                next_position = direction_map[position[0]][position[1]]
+                if next_position == Env.EXIT:
+                    break
+
+                # Check if we move vertical (-10) or if diagonal (-14 (sqrt(2)*10))
+                if position[0] == next_position[0] or position[1] == next_position[1]:
+                    distance -= 10
+                else:
+                    distance -= 14
+
                 gradient_map[position[0]][position[1]] = distance
-                position = direction_map[position[0]][position[1]]
+                position = next_position
 
     return gradient_map
 
 
-
-
-
-for ele in gradient("C:\\Users\\piotr\\Desktop\\CrowdSim\\resources/ready/directios100x100yx.txt"):
+for ele in gradient_from_direction_map("C:\\Users\\piotr\\Desktop\\CrowdSim\\resources/ready/directios100x100yx.txt"):
     print(ele)
