f4efdb5fa4e9b657ecec569b918226b1f13f2891
Milosz Blasiak <milblasiak@gmail.com>
2019-01-22 01:23:12 +0100

    Remove redundant direction_map parameter

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ offset = 20
 # tile size
 t_s = [(w_prev - 2 * (offset + 1)) / len(maze[0]), (h_prev - 2 * (offset + 1)) / len(maze)]
 
-agents = AgentManager(t_s, w_prev, h_prev, offset, directions, exit_points, maze, [direct])
+agents = AgentManager(t_s, w_prev, h_prev, offset, exit_points, maze, [direct])
 agents.add_new([1, 1], random.randint(0, 360), [.0, .0, .6], 0)
 agents.add_new([15, 5], random.randint(0, 360), [.0, .0, .6], 0)
 agents.add_new([90, 70], random.randint(0, 360), [.0, .0, .6], 0)
