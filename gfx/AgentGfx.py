ba7fbbca52672ff457e930c59da70be3ad164fc5
PiotrKedra <piotr.k9750@gmail.com>
2019-01-22 07:53:24 +0100

    Merge branch 'devel' of https://github.com/mblasiak/CrowdMovmentSimulation into pkedra
    # Conflicts:
    #	main.py

--- gfx/AgentGfx.py
+++ gfx/AgentGfx.py
@@ -1,17 +1,18 @@
 from OpenGL.GL import *
 from math import *
+#from model.gradient_agent.AgentGradient import Agent
 from model.agent.Agent import Agent
 
 
 class AgentGfx:
     def __init__(self, position: [float, float], map_position: [int, int], angle: float, color: [float, float, float],
-                 direction, maze, direct):
+                 maze, direct, which_map=0):
         self.map_position = map_position
         self.position = position
         self.angle = radians(angle)
         self.color = color
         exits = list(zip(range(40, 60), [99] * 20))
-        self.agent = Agent((map_position[0], map_position[1]), exits, direct, maze)
+        self.agent = Agent((map_position[0], map_position[1]), exits, direct, maze, which_map)
         self.fx_pos = (0, 0)
 
     def move(self):
